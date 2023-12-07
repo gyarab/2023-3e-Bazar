f0d2a1f64c33fd9c0315cdd88f7ce277a8b16f00
VojtaKadlec <119654682+VojtaKadlec@users.noreply.github.com>
2023-12-07 17:38:37 +0100

    Update homepage.js

--- frontend/homepage.js
+++ frontend/homepage.js
@@ -1,1 +1,10 @@
+function addRectangle() {
+    var rectangle = document.createElement("div");
+    rectangle.style.width = "500px";
+    rectangle.style.height = "50px";
+    rectangle.style.backgroundColor = "black";
+    rectangle.style.margin = "200px";
+    rectangle.style.float = "left";
+    document.body.appendChild(rectangle);
+}
 
