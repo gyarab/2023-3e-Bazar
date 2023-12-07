f85a369ef11e77d653458e1bad4c74fad5c0184e
Jan0007 <94890470+Jan0007@users.noreply.github.com>
2023-12-07 21:05:50 +0100

    7_12_23

---
+++ homepage/static/js/homepage.js
@@ -0,0 +1,13 @@
+function addRectangle() {
+    var rectangle = document.createElement("div");
+    rectangle.style.width = "500px";
+    rectangle.style.height = "200px";
+    rectangle.style.backgroundColor = "black";
+    rectangle.style.margin = "200px";
+    rectangle.style.float = "left";
+    var textSpan = document.createElement("span");// Vytvořte text pro span
+    textSpan.style.color = "white";// Vytvořte text pro span
+    textSpan.textContent = " Lorem ipsum dolor sit amet consectetur adipisicing elit. ";// Vytvořte text pro span
+    rectangle.appendChild(textSpan);
+    document.body.appendChild(rectangle); //připojení
+}
