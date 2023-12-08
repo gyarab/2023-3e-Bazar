d8bee63830f6734cecdf2b7f3214e37fc467e72c
Jan0007 <94890470+Jan0007@users.noreply.github.com>
2023-12-08 09:46:34 +0100

    7_12_23_2

--- homepage/models.py
+++ homepage/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=40)
+    #TODO pridej obrazek
 
     def __str__(self):  
         return self.name
