e5d7f1bc3f57e5bfe0cab8d469fa3bd535dfd093
Jan0007 <94890470+Jan0007@users.noreply.github.com>
2023-12-09 16:13:21 +0100

    9_12_23_2

--- Bazar/urls.py
+++ Bazar/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('homepage.urls'), name='homepage'),
-    path('profilepage/', include('profilepage.urls', name='profilepage'))
+    path('profilepage/', include('profilepage.urls'), name='profilepage')
 ]
