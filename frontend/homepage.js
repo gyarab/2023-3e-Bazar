d909e16c2dafeb720596f7fecbadc1dbeeec83ea
RadimTrilety <93980049+RadimTrilety@users.noreply.github.com>
2023-12-19 17:54:05 +0100

    Update homepage.js

--- frontend/homepage.js
+++ frontend/homepage.js
@@ -3,11 +3,18 @@ function addRectangle() {
     rectangle.style.width = "400px";
     rectangle.style.height = "200px";
     rectangle.style.backgroundColor = "black";
-    rectangle.style.margin = "200px";
+    rectangle.style.marginRight = "20px";  
+    rectangle.style.marginTop = "20px"; 
     rectangle.style.float = "left";
-    var textSpan = document.createElement("span");// Vytvořte text pro span
-    textSpan.style.color = "white";// Vytvořte text pro span
-    textSpan.textContent = " Lorem ipsum dolor sit amet consectetur adipisicing elit. ";// Vytvořte text pro span
+    
+    var textSpan = document.createElement("span");
+    textSpan.style.color = "white";
+    textSpan.textContent = " Lorem ipsum dolor sit amet consectetur adipisicing elit. ";
+    
     rectangle.appendChild(textSpan);
-    document.body.appendChild(rectangle); //připojení
+    document.body.appendChild(rectangle);
+
+    rectangle.addEventListener("click", function() {  // Open a new HTML page when the rectangle is clicked
+        window.location.href = "#";
+    });
 }
